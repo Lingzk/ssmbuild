a3e2e22530d8d07ba9ba20313c416a4851ea424f
Erueka <54078053+Erueka@users.noreply.github.com>
2020-02-15 18:40:54 +0800

    Initial commit

---
+++ src/main/java/com/kuang/service/BookServiceImpl.java
@@ -0,0 +1,41 @@
+package com.kuang.service;
+
+import com.kuang.dao.BookMapper;
+import com.kuang.pojo.Books;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
